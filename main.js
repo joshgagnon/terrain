6e03798f8f0512ca1219c7f107f0921fe4073d9e
Josh Gagnon <josh.n.gagnon@gmail.com>
2023-03-13 14:32:52 +1100

    weighted tiles

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const X_COUNT = 10;
-const Y_COUNT = 10;
+const X_COUNT = 25;
+const Y_COUNT = 25;
 const X_RES = 1000;
 const Y_RES = 1000;
 const SEED = 0;
@@ -43,9 +43,65 @@ const addWeights = (obj1, obj2) => {
     return results;
 }
 
+const selectKeyFromWeighted = (obj) => {
+    let keys = Object.keys(obj);
+    // Calculate the sum of the weights
+    let totalWeight = 0;
+    for (let i = 0; i < keys.length; i++) {
+        totalWeight += obj[keys[i]];
+    }
+    // Generate a random number between 0 and the total weight
+    let randomNumber = rnd() * totalWeight;
+    // Loop through the keys and their weights, subtracting the weight from the random number until it reaches 0
+    for (let i = 0; i < keys.length; i++) {
+        randomNumber -= obj[keys[i]];
+        if (randomNumber <= 0) {
+            return keys[i];
+        }
+    }
+    return null;
+}
+
+function sumCommonKeys(objs) {
+    // Get an array of all the keys in the first object
+    let keys = Object.keys(objs[0]);
+    // Filter the keys to only include those that are present in every object
+    for (let i = 1; i < objs.length; i++) {
+        keys = keys.filter(key => objs[i][key]);
+    }
+    const result = {}
+    for (let i = 0; i < objs.length; i++) {
+        for (let j = 0; j < keys.length; j++) {
+            if(objs[i][keys[j]]) {
+                result[keys[j]] = (result[keys[j]] || 0) + objs[i][keys[j]];
+            }
+        }
+    }
+    return result;
+}
+
+function areWeightsDifferent(obj1, obj2) {
+    // Check if the objects have a different number of properties
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return true;
+    }
+    // Loop through the properties of the first object and check if they differ in the second object
+    for (let prop in obj1) {
+        if (obj1[prop] !== obj2[prop]) {
+            return true;
+        }
+    }
+    // Loop through the properties of the second object and check if they differ in the first object
+    for (let prop in obj2) {
+        if (obj1[prop] !== obj2[prop]) {
+            return true;
+        }
+    }
+    return false
+}
 
 class Cell {
-    possibleTiles = [];
+    possibleTiles = {};
     collapsed = false;
     tile = null;
     constructor(x, y, possibleTiles) {
@@ -53,48 +109,57 @@ class Cell {
         this.y=y;
         this.possibleTiles = possibleTiles;
     }
+
     entropy = () => {
-        return this.possibleTiles.length
+        const sum = Object.keys(this.possibleTiles).reduce((sum, k) => sum + this.possibleTiles[k], 0);
+        const probabilities = Object.values(this.possibleTiles);
+        let entropy = 0;
+        for (let i = 0; i < probabilities.length; i++) {
+            if (probabilities[i] !== 0) {
+                entropy -= (probabilities[i] / sum) * Math.log2(probabilities[i] / sum);
+            }
+        }
+        return entropy;
     }
 
-    update = (newTiles) => {
-        const change = this.possibleTiles.length !== newTiles.length;
-        this.possibleTiles = newTiles;
+    update = (newWeights) => {
+        const change = areWeightsDifferent(this.possibleTiles, newWeights);
+        this.possibleTiles = newWeights;
         this.collapsed = this.entropy() === 1;
         if(this.collapsed) {
-            this.tile = new TILE_MAP[this.possibleTiles[0]];
+            this.tile = new TILE_MAP[Object.keys(this.possibleTiles)[0]];
         }
         return change;
     }
 
     observe = () => {
-        this.possibleTiles = [randomChoice(this.possibleTiles)];
-        this.tile = new TILE_MAP[this.possibleTiles[0]];
+        const newTile = selectKeyFromWeighted(this.possibleTiles);
+        this.tile = new TILE_MAP[newTile];
+        this.possibleTiles = {[newTile]: 1};
         this.collapsed = true;
     }
 
+    getPossibilities = (direction) => {
+        const weights = Object.keys(this.possibleTiles).reduce((acc, tileType) => {
+            return addWeights(acc, TILE_MAP[tileType][direction]);
+        }, {});
+        return weights;
+    }
+
     getAbovePossibilities = () => {
-        return Array.from(new Set(this.possibleTiles.reduce((acc, tileType) => {
-            return [...acc, ...TILE_MAP[tileType].above];
-        }, [])))
+        return this.getPossibilities('above');
     }
 
     getRightPossibilities = () => {
-        return Array.from(new Set(this.possibleTiles.reduce((acc, tileType) => {
-            return [...acc, ...TILE_MAP[tileType].right];
-        }, [])))
+        return this.getPossibilities('right');
     }
 
     getBelowPossibilities = () => {
-        return Array.from(new Set(this.possibleTiles.reduce((acc, tileType) => {
-            return [...acc, ...TILE_MAP[tileType].below];
-        }, [])))
+        return this.getPossibilities('below');
     }
 
     getLeftPossibilities = () => {
-        return Array.from(new Set(this.possibleTiles.reduce((acc, tileType) => {
-            return [...acc, ...TILE_MAP[tileType].left];
-        }, [])))
+        return this.getPossibilities('left');
     }
 }
 
@@ -130,26 +195,23 @@ const VALID_TILE_TYPES = {
     //  SAND: 'SAND',
 }
 
+const DEFAULT_WEIGHTS = Object.keys(VALID_TILE_TYPES).reduce((acc, key) => ({...acc, [key]: 1}),{});
+
 class BaseTile {
     tileType = TILE_TYPES.UNKNOWN;
-    static above = Object.keys(VALID_TILE_TYPES);
-    static right = Object.keys(VALID_TILE_TYPES);
-    static below = Object.keys(VALID_TILE_TYPES);
-    static left = Object.keys(VALID_TILE_TYPES);
-    constructor(x,y) {
-        if(!this.color) {
-            this.colour = `rgba(0,${parseInt(x / X_COUNT * 256, 0)},${parseInt(y / Y_COUNT * 256.)},1)`;
-        }
-    }
+    static above = DEFAULT_WEIGHTS;
+    static right = DEFAULT_WEIGHTS;
+    static below = DEFAULT_WEIGHTS;
+    static left = DEFAULT_WEIGHTS;
 }
 
 class Forest extends BaseTile {
     tileType = TILE_TYPES.FOREST
     colour = '#009900';
-    static above = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
-    static right = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
-    static below = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
-    static left = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
+    static above = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 1};
+    static right = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 1};
+    static below = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] :10, [TILE_TYPES.COAST]: 1};
+    static left = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 1};
 
 }
 
@@ -157,10 +219,10 @@ class Forest extends BaseTile {
 class Plain extends BaseTile {
     tileType = TILE_TYPES.PLAIN;
     colour = '#00FF00';
-    static above = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
-    static right = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
-    static below = [TILE_TYPES.COAST];
-    static left = [TILE_TYPES.FOREST, TILE_TYPES.PLAIN,TILE_TYPES.COAST];
+    static above = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1};
+    static right = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 1};
+    static below = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 10};
+    static left = {[TILE_TYPES.FOREST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.COAST]: 1};
 }
 
 class Hill extends BaseTile {
@@ -181,28 +243,28 @@ class Lake extends BaseTile {
 class Coast extends BaseTile {
     tileType = TILE_TYPES.COAST;
     colour = '#ffed00'
-    static above = [TILE_TYPES.COAST,TILE_TYPES.PLAIN];
-    static right = [TILE_TYPES.COAST, TILE_TYPES.SEA];
-    static below = [TILE_TYPES.COAST,TILE_TYPES.SEA];
-    static left = [TILE_TYPES.COAST, TILE_TYPES.SEA]
+    static above = {[TILE_TYPES.SEA]: 10, [TILE_TYPES.PLAIN]: 1, [TILE_TYPES.FOREST]: 1};
+    static right = {[TILE_TYPES.SEA]: 10, [TILE_TYPES.COAST]: 1, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.FOREST]: 1};
+    static below = {[TILE_TYPES.SEA]: 10, [TILE_TYPES.FOREST]: 1};
+    static left = {[TILE_TYPES.COAST]: 1, [TILE_TYPES.SEA] : 10, [TILE_TYPES.PLAIN] : 1, [TILE_TYPES.FOREST]: 1};
 }
 
 class Sea extends BaseTile {
     tileType = TILE_TYPES.SEA;
     colour = '#0000AA';
-    static above = [TILE_TYPES.COAST, TILE_TYPES.SEA];
-    static right = [TILE_TYPES.COAST, TILE_TYPES.SEA];
-    static below = [TILE_TYPES.OCEAN, TILE_TYPES.SEA];
-    static left = [TILE_TYPES.COAST, TILE_TYPES.SEA]
+    static above = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 3, [TILE_TYPES.COAST]: 1};
+    static right = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 3, [TILE_TYPES.COAST]: 1};
+    static below = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 3, [TILE_TYPES.COAST]: 1};
+    static left = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 3, [TILE_TYPES.COAST]: 1};
 }
 
 class Ocean extends BaseTile {
     tileType = TILE_TYPES.OCEAN;
     colour = '#000055'
-    static above = [TILE_TYPES.OCEAN, TILE_TYPES.SEA];
-    static right = [TILE_TYPES.OCEAN, TILE_TYPES.SEA];
-    static below = [TILE_TYPES.OCEAN, TILE_TYPES.SEA];
-    static left = [TILE_TYPES.OCEAN, TILE_TYPES.SEA]
+    static above = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 16};
+    static right = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 16};
+    static below ={[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 16};
+    static left = {[TILE_TYPES.SEA]: 3, [TILE_TYPES.OCEAN]: 16};
 }
 
 class Desert extends BaseTile {
@@ -245,7 +307,8 @@ class World {
                 if(!this.grid[i]) {
                     this.grid[i] = [];
                 }
-                this.grid[i][j] = new Cell(i, j, Object.keys(TILE_MAP));
+                this.grid[i][j] = new Cell(i, j, Object.keys(VALID_TILE_TYPES)
+                    .reduce((acc, key) => ({...acc, [key]: 1}), {}));
             }
         }
     }
@@ -270,7 +333,7 @@ class World {
                     const y = width * j;
                     ctx.fillStyle = "#000000";
                     ctx.font = `${width/2}px serif`;
-                    ctx.fillText(this.grid[i][j].possibleTiles.length, x + width/2, y+height/2);
+                    ctx.fillText(Object.keys(this.grid[i][j].possibleTiles).length, x + width/2, y+height/2);
                 }
             }
         }
@@ -279,7 +342,7 @@ class World {
     pick() {
         const sortedGrid = this.grid
             .flat()
-            .filter(a => !a.collapsed && a.entropy() >= 1)
+            .filter(a => !a.collapsed)
             .sort((a, b) => a.entropy() - b.entropy());
 
         if(sortedGrid.length === 0) {
@@ -287,6 +350,7 @@ class World {
         }
         const lowestEntropy = sortedGrid[0].entropy();
         const lowestEntropyGrid = sortedGrid.filter(s => s.entropy() === lowestEntropy);
+
         return lowestEntropyGrid[Math.floor(rnd() * lowestEntropyGrid.length)];
     }
 
@@ -298,10 +362,11 @@ class World {
         // collapse our pick
         pick.observe();
         const stack = [pick];
+
         while(stack.length) {
             const item = stack.pop();
-            for (let i = item.x-1; i < item.x+1; i++) {
-                for (let j = item.y-1; j < item.y+1; j++) {
+            for (let i = item.x-1; i <= item.x+1; i++) {
+                for (let j = item.y-1; j <= item.y+1; j++) {
 
                     if(!this.grid[i]?.[j] || this.grid[i][j].collapsed) {
                         continue;
@@ -323,17 +388,18 @@ class World {
                         const left = this.grid[i - 1][j];
                         possibleSets.push(left.getRightPossibilities())
                     }
-                    // get intersection of sets
-                    const validSet = Array.from(new Set(possibleSets.reduce((a, b) => a.filter(c => b.includes(c)))));
+                    const newProbabilities = sumCommonKeys(possibleSets);
 
-                    if(this.grid[i][j].update(validSet)) {
-                        stack.push(this.grid[i][j])
+                    if(this.grid[i][j].update(newProbabilities)) {
+                        this.draw(window._ctx);
+                        stack.push(this.grid[i][j]);
                     }
-
                 }
             }
         }
-        await sleep(100);
+
+        await sleep(3);
+
         return this.collapse();
     }
 }
@@ -359,7 +425,7 @@ async function init() {
 
     const ctx = createCanvas();
     const world = new World();
-
+    window._ctx =ctx;
     window._world = world;
 
     const loop = () => {
